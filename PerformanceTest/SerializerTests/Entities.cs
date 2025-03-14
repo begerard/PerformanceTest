bd640b9d5cf37e3677650e93577b398b0bd38557
Benoit Gerard <benoit.gerard@tackops.com>
2025-03-14 21:28:37 +0100

    Add a test of serializers

---
+++ PerformanceTest/SerializerTests/Entities.cs
@@ -0,0 +1,93 @@
+﻿using PolyType;
+
+namespace SerializerTests;
+
+[GenerateShape<List<A>>]
+partial class ListAWitness;
+
+public class A
+{
+    public bool MyBool { get; set; }
+    public int MyInt { get; set; }
+    public long? MyLong { get; set; }
+    public EnumX MyEnum { get; set; }
+    public TimeSpan MyTimeSpan { get; set; }
+    public TimeOnly MyTime { get; set; }
+    public DateOnly MyDate { get; set; }
+    public string MyString { get; set; }
+
+    public List<B> Childs { get; set; } = [];
+
+    public static List<A> Seed(int nb, bool circular)
+    {
+        var result = new List<A>();
+        for (int i = 0; i < nb; i++)
+        {
+            A a = new();
+            a.MyString = RandomString(nb);
+            result.Add(a);
+
+            var bytesArray = new byte[nb];
+            Random.Shared.NextBytes(bytesArray);
+            a.MyString = Convert.ToBase64String(bytesArray);
+
+            for (int j = 0; j < nb; j++)
+            {
+                B b = new();
+                b.MyString = RandomString(nb);
+                a.Childs.Add(b);
+
+                if (circular) b.Parent = a;
+
+                for (int k = 0; k < nb; k++)
+                {
+                    C c = new();
+                    c.MyString = RandomString(nb);
+                    b.Childs.Add(c);
+
+                    if (circular) c.Parent = b;
+                }
+            }
+        }
+
+        return result;
+    }
+
+    private static string RandomString(int lenght)
+    {
+        var bytesArray = new byte[lenght];
+        Random.Shared.NextBytes(bytesArray);
+        return Convert.ToBase64String(bytesArray);
+    }
+}
+
+public class B
+{
+    public bool MyBool { get; set; }
+    public int MyInt { get; set; }
+    public long? MyLong { get; set; }
+    public EnumX MyEnum { get; set; }
+    public TimeSpan MyTimeSpan { get; set; }
+    public TimeOnly MyTime { get; set; }
+    public DateOnly MyDate { get; set; }
+    public string MyString { get; set; }
+
+    public List<C> Childs { get; set; } = [];
+    public A Parent { get; set; }
+}
+
+public class C
+{
+    public bool MyBool { get; set; }
+    public int MyInt { get; set; }
+    public long? MyLong { get; set; }
+    public EnumX MyEnum { get; set; }
+    public TimeSpan MyTimeSpan { get; set; }
+    public TimeOnly MyTime { get; set; }
+    public DateOnly MyDate { get; set; }
+    public string MyString { get; set; }
+
+    public B Parent { get; set; }
+}
+
+public enum EnumX { None, One, Two, Three }
---
+++ PerformanceTest/SerializerTests/Serialize.cs
@@ -0,0 +1,108 @@
+﻿using BenchmarkDotNet.Attributes;
+using Ceras;
+using Nerdbank.MessagePack;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace SerializerTests;
+
+[MemoryDiagnoser(false)]
+public class Serialize
+{
+    private List<A> data;
+    private List<A> dataCycle;
+
+    private JsonSerializerOptions jsonOptions;
+    private MessagePackSerializer messagePackSerializer;
+    private CerasSerializer cerasSerializer;
+
+    private JsonSerializerOptions jsonOptionsWithReference;
+    private MessagePackSerializer messagePackSerializerWithReference;
+    private CerasSerializer cerasSerializerWithReference;
+
+    [GlobalSetup]
+    public void GlobalSetup()
+    {
+        data = A.Seed(10, false);
+        dataCycle = A.Seed(10, true);
+
+        jsonOptions = new JsonSerializerOptions() { ReferenceHandler = null };
+        messagePackSerializer = new MessagePackSerializer() { PreserveReferences = false };
+        cerasSerializer = new CerasSerializer(new() { PreserveReferences = false });
+        CerasBufferPool.Pool = new CerasDefaultBufferPool();
+
+        jsonOptionsWithReference = new JsonSerializerOptions() { ReferenceHandler = ReferenceHandler.Preserve };
+        messagePackSerializerWithReference = new MessagePackSerializer() { PreserveReferences = true };
+        cerasSerializerWithReference = new CerasSerializer(new() { PreserveReferences = true });
+    }
+
+    [Benchmark]
+    public void StjWithoutReference()
+    {
+        var serialized = JsonSerializer.Serialize(data, jsonOptions);
+        var deserialized = JsonSerializer.Deserialize<List<A>>(serialized, jsonOptions);
+        if (deserialized.Count == 0) throw new InvalidOperationException();
+    }
+    [Benchmark]
+    public void MsgPackWithoutReference()
+    {
+        var serialized = messagePackSerializer.Serialize<List<A>, ListAWitness>(data);
+        var deserialized = messagePackSerializer.Deserialize<List<A>, ListAWitness>(serialized);
+        if (deserialized.Count == 0) throw new InvalidOperationException();
+    }
+    [Benchmark]
+    public void CerasWithoutReference()
+    {
+        var serialized = cerasSerializer.Serialize(data);
+        var deserialized = cerasSerializer.Deserialize<List<A>>(serialized);
+        if (deserialized.Count == 0) throw new InvalidOperationException();
+    }
+
+
+
+    [Benchmark]
+    public void StjWithReference()
+    {
+        var serialized = JsonSerializer.Serialize(data, jsonOptionsWithReference);
+        var deserialized = JsonSerializer.Deserialize<List<A>>(serialized, jsonOptionsWithReference);
+        if (deserialized.Count == 0) throw new InvalidOperationException();
+    }
+    [Benchmark]
+    public void MsgPackWithReference()
+    {
+        var serialized = messagePackSerializerWithReference.Serialize<List<A>, ListAWitness>(data);
+        var deserialized = messagePackSerializerWithReference.Deserialize<List<A>, ListAWitness>(serialized);
+        if (deserialized.Count == 0) throw new InvalidOperationException();
+    }
+    [Benchmark]
+    public void CerasWithReference()
+    {
+        var serialized = cerasSerializerWithReference.Serialize(data);
+        var deserialized = cerasSerializerWithReference.Deserialize<List<A>>(serialized);
+        if (deserialized.Count == 0) throw new InvalidOperationException();
+    }
+
+
+
+    [Benchmark]
+    public void StjWithCycle()
+    {
+        var serialized = JsonSerializer.Serialize(dataCycle, jsonOptionsWithReference);
+        var deserialized = JsonSerializer.Deserialize<List<A>>(serialized, jsonOptionsWithReference);
+        if (deserialized.Count == 0) throw new InvalidOperationException();
+    }
+    //[Benchmark]
+    //public void MsgPackWithCycle()
+    //{
+    //    var serialized = messagePackSerializerWithReference.Serialize<List<A>, ListAWitness>(dataCycle);
+    //    var deserialized = messagePackSerializerWithReference.Deserialize<List<A>, ListAWitness>(serialized);
+    //    if (deserialized.Count == 0) throw new InvalidOperationException();
+    //}
+    [Benchmark]
+    public void CerasWithCycle()
+    {
+        var serialized = cerasSerializerWithReference.Serialize(dataCycle);
+        var deserialized = cerasSerializerWithReference.Deserialize<List<A>>(serialized);
+        if (deserialized.Count == 0) throw new InvalidOperationException();
+    }
+}
